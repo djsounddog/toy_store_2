9cb22ae887b40d3c74a31d29915b9a5284fd483c
Joshua Gilmore <djsounddog@gmail.com>
2020-04-28 21:44:22 +1000

    Added category for toys.

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -4,4 +4,6 @@ class Toy < ApplicationRecord
   belongs_to :user
   has_one :manufacturer
   has_one_attached :image
+  has_many :toys_categories
+  has_many :categories, through: :toys_categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_053023) do
+ActiveRecord::Schema.define(version: 2020_04_28_112006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2020_04_27_053023) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "manufacturers", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -56,6 +62,15 @@ ActiveRecord::Schema.define(version: 2020_04_27_053023) do
     t.index ["user_id"], name: "index_toys_on_user_id"
   end
 
+  create_table "toys_categories", force: :cascade do |t|
+    t.bigint "toy_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_toys_categories_on_category_id"
+    t.index ["toy_id"], name: "index_toys_categories_on_toy_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
@@ -66,4 +81,6 @@ ActiveRecord::Schema.define(version: 2020_04_27_053023) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "toys", "manufacturers"
   add_foreign_key "toys", "users"
+  add_foreign_key "toys_categories", "categories"
+  add_foreign_key "toys_categories", "toys"
 end
