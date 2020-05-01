06cb7160c46818e72b3b3300af6c239f16fba790
Joshua Gilmore <djsounddog@gmail.com>
2020-05-01 16:54:57 +1000

    Repaired seeds file to work with devise.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'test@test.com', password: '123456')
+user = User.new
+user.email = 'test@test.com'
+user.password = '123456'
+user.save!
 
 3.times do
-  user = User.create(
-    email: Faker::Internet.email,
-    password: '123456'
-  )
+  user = User.new
+  user.email = Faker::Internet.email
+  user.password = '123456'
+  user.save!
 end
 
 4.times do
